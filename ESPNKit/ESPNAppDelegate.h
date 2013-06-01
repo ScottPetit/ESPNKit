24d735aaf72e18c750b44604f5e7e7c94dd395b4
Scott Petit <scott.petit@bottlerocketapps.com>
2013-06-01 09:09:31 -0500

    Initial commit - Project init

---
+++ ESPNKit/ESPNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESPNAppDelegate.h
+//  ESPNKit
+//
+//  Created by Scott Petit on 6/1/13.
+//  Copyright (c) 2013 Scott Petit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESPNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ESPNKit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ESPNKit
+//
+//  Created by Scott Petit on 6/1/13.
+//  Copyright (c) 2013 Scott Petit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESPNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESPNAppDelegate class]));
+    }
+}
