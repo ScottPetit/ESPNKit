aefbf45a24362a20b1d14e51618d9aa20bfe5c8c
Scott Petit <scott.petit@bottlerocketapps.com>
2013-06-02 13:34:17 -0500

    - adds ESPNLeagues constants

---
+++ ESPNKit/Models/ESPNLeagues.h
@@ -0,0 +1,54 @@
+//
+//  ESPNLeagues.h
+//  ESPNKit
+//
+//  Created by Scott Petit on 6/1/13.
+//  Copyright (c) 2013 Scott Petit. All rights reserved.
+//
+
+#ifndef ESPNKit_ESPNLeagues_h
+#define ESPNKit_ESPNLeagues_h
+
+static NSString * const ESPNLeagueMLB = @"baseball/mlb";
+static NSString * const ESPNLeagueMensCollegeBasketball = @"basketball/mens-college-basketball";
+static NSString * const ESPNLeagueNBA = @"basketball/nba";
+static NSString * const ESPNLeagueWomensCollegeBasketball = @"basketball/womens-college-basketball";
+static NSString * const ESPNLeagueCollegeFootball = @"football/college-football";
+static NSString * const ESPNLeagueNFL = @"football/nfl";
+static NSString * const ESPNLeagueGolf = @"golf";
+static NSString * const ESPNLeagueEuropeanTourGolf = @"golf/eur";
+static NSString * const ESPNLeagueNationwideTourGolf = @"golf/ntw";
+static NSString * const ESPNLeaguePGA = @"golf/pga";
+static NSString * const ESPNLeagueSGA = @"golf/sga";
+static NSString * const ESPNLeagueNHL = @"hockey/nhl";
+static NSString * const ESPNLeagueF1 = @"racing/f1";
+static NSString * const ESPNLeagueIRL = @"racing/irl";
+static NSString * const ESPNLeagueNascar = @"racing/nascar";
+static NSString * const ESPNLeaguePrimeraArgentina = @"soccer/arg.1";
+static NSString * const ESPNLeagueChampionsLeague = @"soccer/uefa.champions";
+static NSString * const ESPNLeagueWorldCup = @"soccer/fifa.world";
+static NSString * const ESPNLeagueAustrianBundesliga = @"soccer/aut.1";
+static NSString * const ESPNLeagueBelgianJuplier = @"soccer/bel.1";
+static NSString * const ESPNLeagueDanishSASLigaen = @"soccer/den.1";
+static NSString * const ESPNLeagueFrenchLigueOne = @"soccer/fra.1";
+static NSString * const ESPNLeagueGermanBundesliga = @"soccer/ger.1";
+static NSString * const ESPNLeagueDutchEredivise = @"soccer/ned.1";
+static NSString * const ESPNLeagueItalianSerieA = @"soccer/ita.1";
+static NSString * const ESPNLeagueNorwegianTippeligaen = @"soccer/nor.1";
+static NSString * const ESPNLeaguePortugueseLiga = @"soccer/por.1";
+static NSString * const ESPNLeagueSpanishPrimera = @"soccer/esp.1";
+static NSString * const ESPNLeagueSwedishAllsvenskanliga = @"soccer/swe.1";
+static NSString * const ESPNLeagueSwissSuperLeague = @"soccer/sui.1";
+static NSString * const ESPNLeagueTurkishSuperLig = @"soccer/tur.1";
+static NSString * const ESPNLeagueMLS = @"soccer/usa.1";
+static NSString * const ESPNLeagueUSWomensUnitedSoccerAssociation = @"soccer/usa.w.1";
+static NSString * const ESPNLeagueMexicanLiga = @"soccer/mex.1";
+static NSString * const ESPNLeagueBarclaysPremierLeague = @"soccer/eng.1";
+static NSString * const ESPNLeagueEnglishLeagueChampionship = @"soccer/eng.2";
+static NSString * const ESPNLeagueTennisProfessionals = @"tennis/atp";
+static NSString * const ESPNLeagueWomensTennis = @"tennis/wta";
+static NSString * const ESPNLeagueBoxing = @"boxing";
+static NSString * const ESPNLeagueMMA = @"mma";
+static NSString * const ESPNLeagueHorseRacing = @"horse-racing";
+
+#endif
