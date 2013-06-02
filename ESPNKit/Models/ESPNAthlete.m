62cd4ba7436f746ed11cfa7a0f7dcccb85bf65ec
Scott Petit <scott.petit@bottlerocketapps.com>
2013-06-02 13:32:37 -0500

    - starts setting some properties of Athlete

--- ESPNKit/Models/ESPNAthlete.m
+++ ESPNKit/Models/ESPNAthlete.m
@@ -10,4 +10,29 @@
 
 @implementation ESPNAthlete
 
+- (id)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super initWithDictionary:dictionary];
+    if (self)
+    {
+        self.firstName = dictionary[@"firstName"];
+        self.lastName = dictionary[@"lastName"];
+        self.fullName = dictionary[@"fullName"];
+        self.displayName = dictionary[@"displayName"];
+        self.shortName = dictionary[@"shortName"];
+        self.weight = [dictionary[@"weight"] integerValue];
+        self.height = [dictionary[@"height"] integerValue];
+        self.hand = dictionary[@"hand"];
+        self.jerseyNumber = [dictionary[@"jersey"] stringValue];
+        self.dateOfBirth = dictionary[@"dateOfBirth"];
+        self.age = [dictionary[@"age"] integerValue];
+    }
+    return self;
+}
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"<%@: %p, \"%@ %@\">", [self class], self, self.firstName, self.lastName];
+}
+
 @end
